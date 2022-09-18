0de891a2081442ac4d14b6832260c6c4ad9eccd4
RemRem <remrem@github.com>
2022-09-18 15:39:58 +0200

    add save_modif + add_birthday + menu

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import json
 import datetime
 
-f = open('birthday.json','r')
+f = open('birthday.json','r+')
 data = json.load(f)
 today = datetime.date.today()
+
+def menu():
+    x = input("0 -> Check Birthday\n1 -> Add Birthday\n--> ")
+    if (x=="0"):
+        string_to_date(data)
+        search_birthday(data)
+        return
+    else:
+        ask_birthday(data)
+
+def ask_birthday(birthday_list):
+    dict = {}
+    firstname = input("Prénom: ")
+    lastname = input("Nom: ")
+    dob = input("Date de naissance: ")
+    dict["firstname"] = firstname
+    dict["lastname"] = lastname
+    dict["dob"] = dob
+    add_birthday(data, dict)
+
+def add_birthday(birthday_list, new_bd):
+    birthday_list.append(new_bd)
+    save_modif(birthday_list)
 
 def is_birthday(today, dob):
     if today.month == dob.month and today.day == dob.day:
@@ -22,5 +45,15 @@ def search_birthday(birthday_list):
     if found == False:
         print('Aucun anniversaire aujourd\'hui !')
 
-string_to_date(data)
-search_birthday(data)
+def print_json(birthday_list):
+    print(birthday_list)
+
+def save_modif(data):
+    with open('birthday.json', 'w') as f:
+        json.dump(data, f)
+
+
+menu()
+print_json(data)
+f.close
+
